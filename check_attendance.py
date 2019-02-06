cb36c4f4e4819d2dbe3d3c0e2eb5bcf885c83fd6
Emmet Young <death-droid@hotmail.com>
2019-02-06 16:23:47 +1100

    Initial commit

---
+++ check_attendance.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+import time
+import RPi.GPIO as GPIO
+import SimpleMFRC522
+import mysql.connector
+import Adafruit_CharLCD as LCD
+
+db = mysql.connector.connect(
+  host="localhost",
+  user="attendanceadmin",
+  passwd="pimylifeup",
+  database="attendancesystem"
+)
+
+cursor = db.cursor()
+reader = SimpleMFRC522.SimpleMFRC522()
+
+lcd = LCD.Adafruit_CharLCD(4, 24, 23, 17, 18, 22, 16, 2, 4);
+
+try:
+  while True:
+    lcd.clear()
+    lcd.message('Place Card to\nrecord attendance')
+    id, text = reader.read()
+
+    cursor.execute("Select id, name FROM users WHERE rfid_uid="+str(id))
+    result = cursor.fetchone()
+
+    lcd.clear()
+
+    if cursor.rowcount != 0:
+      lcd.message("Welcome " + result[1])
+      cursor.execute("INSERT INTO attendance (user_id) VALUES (%s)", (result[0],) )
+      db.commit()
+    else:
+      lcd.message("User does not exist.")
+    time.sleep(2)
+finally:
+  GPIO.cleanup()
+
